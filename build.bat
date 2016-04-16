df43977a828e29fddb2f81a43ddfb608667f706d
Mramaizng <beersbr@gmail.com>
2016-04-15 20:04:43 -0700

    first commit

---
+++ build.bat
@@ -0,0 +1,9 @@
+
+# call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x64
+
+pushd src
+
+
+cl main.cpp /I "../build/src" /MT /link /SUBSYSTEM:CONSOLE /LIBPATH:../build/lib opengl32.lib SDL2.lib SDL2main.lib /out:../binary/ludum_dare_35.exe
+
+popd
---
+++ src/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+#include <SDL2/SDL.h>
+
+#define WINDOW_WIDTH 1200
+#define WINDOW_HEIGHT 800
+
+int main(int argc, char* argv[]) {
+
+	if(SDL_Init(SDL_INIT_EVERYTHING) < 0)
+	{
+		std::cout << "SOMETHING WENT WRONG!!!" << std::endl;
+	}
+
+	SDL_Window *mainWindow = SDL_CreateWindow("ludum_dare_35",
+	                                          SDL_WINDOWPOS_CENTERED,
+	                                          SDL_WINDOWPOS_CENTERED,
+	                                          WINDOW_WIDTH,
+	                                          WINDOW_HEIGHT,
+	                                          SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
+
+	if(!mainWindow) {
+		return -1;
+	}
+
+	bool running = true;
+
+	SDL_Event event;
+
+	while(running) {
+		while(SDL_PollEvent(&event)){}
+	}
+
+
+	return 0;
+}
