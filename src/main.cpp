fdea8421f1f23634fe2899db42a041b9bcecf048
Mramaizng <beersbr@gmail.com>
2016-04-15 20:32:54 -0700

    opengl working

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_opengl.h>
 
 #define WINDOW_WIDTH 1200
 #define WINDOW_HEIGHT 800
@@ -20,15 +21,35 @@ int main(int argc, char* argv[]) {
 	                                          SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
 
 	if(!mainWindow) {
-		return -1;
+		std::cout << "Could not create window" << std::endl;
 	}
 
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
+	SDL_GLContext mainContext = SDL_GL_CreateContext(mainWindow);
+
+
+	if(!mainContext){
+		std::cout << "Could not init gl" << std::endl;
+	}
+
+
+	SDL_GL_SetSwapInterval(1);
+
 	bool running = true;
-
 	SDL_Event event;
-
 	while(running) {
-		while(SDL_PollEvent(&event)){}
+		while(SDL_PollEvent(&event)) {
+
+		}
+
+		glClearColor(1.0, 0.0, 1.0, 1.0);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		SDL_GL_SwapWindow(mainWindow);
 	}
 
 
